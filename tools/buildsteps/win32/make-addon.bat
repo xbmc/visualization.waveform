61fb92e91681f1630c1b043d768e44a046af353d
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2013-11-03 20:08:08 +0100

    remove outdated stuff

--- tools/buildsteps/win32/make-addon.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-CLS
-call "%VS100COMNTOOLS%..\..\VC\bin\vcvars32.bat"
-IF EXIST %WORKSPACE%\build rmdir %WORKSPACE%\build /S /Q
-mkdir %WORKSPACE%\build
-cd %WORKSPACE%\build
-cmake -G "NMake Makefiles" -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release -DXBMC_BINDINGS=%XBMC_BINDINGS% ..
-cmake --build . --target PACKAGE
