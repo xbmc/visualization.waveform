afe01d8007e84171f8d8516901ee8b7453ffff6b
Alwin Esch <alwin.esch@web.de>
2019-10-07 20:02:49 +0200

    update source licence years and names

--- src/Main_dx.cpp
+++ src/Main_dx.cpp
@@ -1,6 +1,6 @@
 /*
- *      Copyright (C) 2008-2013 Team XBMC
- *      http://xbmc.org
+ *      Copyright (C) 2008-2019 Team Kodi
+ *      http://kodi.tv
  *
  *  This Program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -13,7 +13,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with XBMC; see the file COPYING.  If not, see
+ *  along with Kodi; see the file COPYING.  If not, see
  *  <http://www.gnu.org/licenses/>.
  *
  */
@@ -128,7 +128,7 @@ ADDON_STATUS CVisualizationWaveForm::Create()
 }
 
 //-- Audiodata ----------------------------------------------------------------
-// Called by XBMC to pass new audio data to the vis
+// Called by Kodi to pass new audio data to the vis
 //-----------------------------------------------------------------------------
 void CVisualizationWaveForm::AudioData(const float* pAudioData, int iAudioDataLength, float *pFreqData, int iFreqDataLength)
 {
